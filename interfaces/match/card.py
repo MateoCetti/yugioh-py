f9d95ec2c7d980eaa43c7730d7a1669d4b084027
Mateo Cetti <mateocetti2000@gmail.com>
2025-03-19 22:32:26 -0300

    Make card interface a class

--- interfaces/match/card.py
+++ interfaces/match/card.py
@@ -1,9 +1,26 @@
 import pygame
 
 
-def draw_front_straight_card(screen, xy: pygame.Vector2, card_image, resize):
-    image = pygame.image.load(card_image)
-    scaled_image = pygame.transform.scale(
-        image, (image.get_width() *resize, image.get_height() *resize)
-    )
-    screen.blit(scaled_image, xy)
+class CardInterface:
+    def __init__(self, image, xy: pygame.Vector2):
+        self.image = pygame.image.load(image)
+        self.xy = xy
+        self.wh = pygame.Vector2(
+            self.image.get_width(), self.image.get_height()
+        )  # width and height
+
+    def draw_straight(self, screen, resize):
+        scaled_image = pygame.transform.scale(
+            self.image,
+            (self.image.get_width() * resize, self.image.get_height() * resize),
+        )
+        screen.blit(scaled_image, self.xy)
+
+    def check_if_inside(self, x, y):
+        if (
+            x > self.xy.x
+            and x < self.xy.x + self.wh.x
+            and y > self.xy.y
+            and y < self.xy.y + self.wh.y
+        ):
+            self.xy = pygame.Vector2(x, y)
