59b91beaa16e5bbc769eb0678a59b15eb48883bc
Mateo Cetti <mateocetti2000@gmail.com>
2025-03-19 22:06:08 -0300

    refactor: use pygame vector2

--- interfaces/match/card.py
+++ interfaces/match/card.py
@@ -1,9 +1,9 @@
 import pygame
 
 
-def draw_front_straight_card(screen, x, y, card_image, resize):
+def draw_front_straight_card(screen, xy: pygame.Vector2, card_image, resize):
     image = pygame.image.load(card_image)
     scaled_image = pygame.transform.scale(
         image, (image.get_width() *resize, image.get_height() *resize)
     )
-    screen.blit(scaled_image, (x, y))
+    screen.blit(scaled_image, xy)
--- interfaces/match/hand.py
+++ interfaces/match/hand.py
@@ -6,5 +6,7 @@ def draw_hand(screen, hand):
     initial_xy = pygame.Vector2(500, 750)
     total_cards = len(hand)
     for index, card in enumerate(hand):
-        current_xy = pygame.Vector2(initial_xy.x + (400 * index)/total_cards, initial_xy.y)
-        draw_front_straight_card(screen, current_xy.x, current_xy.y, card.image, 0.2)
+        current_xy = pygame.Vector2(
+            initial_xy.x + (400 * index) / total_cards, initial_xy.y
+        )
+        draw_front_straight_card(screen, current_xy, card.image, 0.2)
